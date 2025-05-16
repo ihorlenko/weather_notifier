e098cbdc48f3db70395f4e0106eda2c6edc86e99
Ihor <>
2025-05-17 01:53:27 +0300

    refactor(be): load config, migrate, connect to db with gorm

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,23 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/ihorlenko/weather_notifier/internal/api"
+	"github.com/ihorlenko/weather_notifier/internal/config"
+	"github.com/ihorlenko/weather_notifier/internal/database"
 )
 
 func main() {
+
+	cfg := config.LoadConfig()
+
+	if err := database.RunMigrations(cfg); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+
+	_, err := database.NewDBConnection(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
 	router := gin.Default()
 
 	api.SetupRoutes(router)
