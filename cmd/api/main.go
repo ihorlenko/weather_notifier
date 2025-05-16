1aa61517f0f8bcdcbbd7faa31f508c1dfce9d653
Ihor <>
2025-05-17 01:54:04 +0300

    chore(be): retrieve port from config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,8 +26,7 @@ func main() {
 
 	api.SetupRoutes(router)
 
-	log.Println("Starting server on :8080")
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(":" + cfg.AppConfig.Port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
