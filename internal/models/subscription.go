d8c9c8499bd45d27f8f5c3da46c45f195f9c87a3
Ihor <>
2025-05-19 00:31:15 +0300

    fix(be): change frequency type to string

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -7,7 +7,7 @@ type Subscription struct {
 	UserID            uint   `gorm:"not null"`
 	User              User   `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 	City              string `gorm:"not null"`
-	Frequency         int    `gorm:"not null"`
+	Frequency         string `gorm:"not null"`
 	Status            string `gorm:"not null"`
 	ConfirmationToken string `gorm:"not null"`
 	UnsubscribeToken  string `gorm:"not null"`
